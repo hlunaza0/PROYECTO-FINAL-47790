c09534124244f4f6c9e6ad585eabd3715e5b1b92
hans <hlunaza@gmail.com>
2023-11-23 04:10:07 -0500

    Actualizacion v.0.1

--- ProyectoFinal/AppCoder/urls.py
+++ ProyectoFinal/AppCoder/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
 from AppCoder.views import inicio_view, cursos_view
 
-from django.http import HttpResponse
-
-def inicio_view(xx):
-    return HttpResponse("Bienvenidos ...!!!!!")
-
-def cursos_view(xx):
-    return HttpResponse("Aqui voy a mostrar los mis cursos")
-
 urlpatterns = [
-    path('inicio/', inicio_view),
+    path("inicio/", inicio_view),
     path("cursos/", cursos_view)
 ]
 
